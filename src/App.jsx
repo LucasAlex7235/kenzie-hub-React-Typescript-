91f3596f2f575948e0d59397f1f9f084d762cfb4
Lucas <lucasalex7235@gmail.com>
2022-10-13 10:24:26 -0300

    feat: add techs / todoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { ToastContainer } from "react-toastify";
 import { AuthValidation } from "./context/Auth/AuthContext";
 import { DashboardModal } from "./context/Dashboard/DashboardContext";
-// import { LoginUserContext } from "./context/Login/";
 import { RoutesUrl } from "./Routes";
 const App = () => {
   return (
